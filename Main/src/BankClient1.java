b0ecfe8b7f9f8389ac35bdc04da9c219156e78da
Yad <yadkarim99@gmail.com>
2021-11-04 23:55:56 +0000

    Creating server and client
    Creating client and server classes, testing thread running

---
+++ Main/src/BankClient1.java
@@ -0,0 +1,5 @@
+public class UserClient1 extends Thread {
+    public void run() {
+        System.out.println("UserClient1 is running");
+    }
+}
---
+++ Main/src/BankServerThread.java
@@ -0,0 +1,10 @@
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+
+    Socket
+
+    public void run() {
+        System.out.println("UserClient1 is running");
+    }
+}
